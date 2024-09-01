f47b358ec2781f3440a61c3cad254dcd2dd40b71
Pedro Andrade <pehandrade.97@gmail.com>
2024-09-01 20:35:54 -0300

    Projeto criado

---
+++ backend/src/test/java/com/projetoimpacta/webmovie/WebmovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetoimpacta.webmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebmovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
