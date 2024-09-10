076114326a5086fb76dd64461d1c031c17d4eae4
Pedro Andrade <pehandrade.97@gmail.com>
2024-09-10 16:17:26 -0300

    first request

--- backend/src/main/java/com/projetoimpacta/webmovie/controllers/ScoreController.java
+++ backend/src/main/java/com/projetoimpacta/webmovie/controllers/ScoreController.java
@@ -13,7 +13,7 @@ import com.projetoimpacta.webmovie.services.ScoreService;
 @RestController
 @RequestMapping(value = "/scores")
 public class ScoreController {
-
+	
 	@Autowired
 	private ScoreService service;
 
