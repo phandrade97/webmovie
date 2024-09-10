f223e588c572349263a77965b13615e49c7f155f
Pedro Andrade <pehandrade.97@gmail.com>
2024-09-10 19:37:51 -0300

    salvar score

--- frontend/src/components/FormCard/index.tsx
+++ frontend/src/components/FormCard/index.tsx
@@ -2,8 +2,9 @@ import { Link } from 'react-router-dom';
 import { Movie } from 'types/movie';
 import './styles.css';
 import { useEffect, useState } from 'react';
-import axios from 'axios';
+import axios, { AxiosRequestConfig} from 'axios';
 import { BASE_URL } from 'utils/request';
+import { validateEmail } from 'utils/validate';
 
 type Props = {
     movieId : string;
@@ -22,12 +23,38 @@ function FormCard( { movieId } : Props) {
       });
     }, [movieId]);
 
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        const email = (event.target as any).email.value;
+        const score = (event.target as any).score.value;
+    
+        if (!validateEmail(email)) {
+            return;
+        }
+
+        const config: AxiosRequestConfig = {
+            baseURL: BASE_URL,
+            method: 'PUT',
+            url: '/scores',
+            data: {
+                email: email,
+                movieId: movieId,
+                score: score
+            }
+        }
+
+        axios(config).then(response => {
+            console.log(response.data);
+        });
+    }
+
+
     return (
         <div className="webmovie-form-container">
             <img className="webmovie-movie-card-image" src={movie?.image} alt={movie?.titulo} />
             <div className="webmovie-card-bottom-container">
                 <h3>{movie?.titulo}</h3>
-                <form className="webmovie-form">
+                <form className="webmovie-form" onSubmit={handleSubmit}>
                     <div className="form-group webmovie-form-group">
                         <label htmlFor="email">Informe seu email</label>
                         <input type="email" className="form-control" id="email" />
