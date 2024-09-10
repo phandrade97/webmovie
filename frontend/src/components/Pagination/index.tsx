78d3b243561e94cb3a3552ff0010d8fe5a000f96
Pedro Andrade <pehandrade.97@gmail.com>
2024-09-10 18:32:00 -0300

    teste paginacao

--- frontend/src/components/Pagination/index.tsx
+++ frontend/src/components/Pagination/index.tsx
@@ -1,23 +1,29 @@
 
 import { ReactComponent as Arrow} from 'assets/img/arrow.svg';
 import './styles.css';
+import { MoviePage } from 'types/movie';
 
-function Pagination(){
+type Props = {
+    page: MoviePage;
+    onChange: Function;
+}
+
+
+function Pagination({ page, onChange } : Props){
     return (
-
         <div className="webmovie-pagination-container">
             <div className="webmovie-pagination-box">
-            < button className="webmovie-pagination-button" disabled={true}  >
+            < button className="webmovie-pagination-button" 
+                disabled={page.first}  onClick={() => onChange(page.number - 1)}>
                 <Arrow />
             </button>
-            <p>{`${1} de ${3}`}</p>
-            <button className="webmovie-pagination-button" disabled={false} >
+            <p>{`${page.number + 1} de ${page.totalPages}`}</p>
+            <button className="webmovie-pagination-button" 
+            disabled={page.last} onClick={() => onChange(page.number + 1)} >
                 <Arrow className="webmovie-flip-horizontal" />
             </button>
             </div>
         </div>
-    
-
     );
 
 }
