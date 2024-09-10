bb0df2826fda78a2bed7fe948c55f1654615d5d3
Pedro Andrade <pehandrade.97@gmail.com>
2024-09-10 17:45:13 -0300

    parametros front

--- frontend/src/pages/Form/index.tsx
+++ frontend/src/pages/Form/index.tsx
@@ -1,47 +1,11 @@
-import { Link } from 'react-router-dom';
-import './styles.css';
+import FormCard from '../../components/FormCard';
+import { useParams } from 'react-router-dom';
 
 function Form() {
-
-    const movie = {
-        id: 1,
-        image: "https://media.themoviedb.org/t/p/w220_and_h330_face/vb4T4VSJ38XdFI9RYS2QB6CCJmd.jpg",
-        title: "Twisters",
-        count: 1,
-        score: 3.5
-
-    };
-
+    const params = useParams();
 
     return (
-        <div className="webmovie-form-container">
-            <img className="webmovie-movie-card-image" src={movie.image} alt={movie.title} />
-            <div className="webmovie-card-bottom-container">
-                <h3>{movie.title}</h3>
-                <form className="webmovie-form">
-                    <div className="form-group webmovie-form-group">
-                        <label htmlFor="email">Informe seu email</label>
-                        <input type="email" className="form-control" id="email" />
-                    </div>
-                    <div className="form-group webmovie-form-group">
-                        <label htmlFor="score">Informe sua avaliação</label>
-                        <select className="form-control" id="score">
-                            <option>1</option>
-                            <option>2</option>
-                            <option>3</option>
-                            <option>4</option>
-                            <option>5</option>
-                        </select>
-                    </div>
-                    <div className="webmovie-form-btn-container">
-                        <button type="submit" className="btn btn-primary webmovie-btn">Salvar</button>
-                        <Link to="/">
-                            <button className="btn btn-primary webmovie-btn mt-2">Cancelar</button>
-                        </Link>
-                    </div>
-                </form>
-            </div>
-        </div>
+        <FormCard movieId={`${params.movieId}`} />
     );
 }
 
